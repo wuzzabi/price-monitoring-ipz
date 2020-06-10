0114c994bc613eb4d616aca806c25f4dee0b18b0
wuzzabi <vova.sabaev21@gmail.com>
2020-06-10 21:33:44 +0300

    Add ts to packagejson and test for cartset

--- server.ts
+++ server.ts
@@ -1,67 +1,12 @@
-import 'module-alias/register'
-import * as express from 'express'
-import * as session from 'express-session'
-import * as helmet from 'helmet'
-import * as compression from 'compression'
-import * as bodyParser from 'body-parser'
-import * as hpp from 'hpp'
-import authMiddleware from '@middlewares/auth.middleware'
+import App from './app'
 import AuthRoute from '@routes/auth.route'
 import CartRoute from '@routes/cart.route'
 import CartSetRoute from '@routes/cartset.route'
-import 'dotenv/config'
 
-class Server {
+const app = new App([
+    new AuthRoute,
+    new CartRoute,
+    new CartSetRoute
+])
 
-    public app: express.Application
-
-    constructor() {
-
-        this.app = express()
-
-        this.initializeMiddlewares()
-        this.initializeRouting()
-    }
-
-    private initializeMiddlewares(): void {
-
-        // Setup common security protection
-        this.app.use(helmet())
-
-        // Setup requests gzip compression
-        this.app.use(compression())
-
-        // Session settings
-        this.app.use(session({
-            secret: process.env.COOKIE_SECRET,
-            resave: false,
-            cookie: { maxAge: 8 * 60 * 60 * 1000 },  // 8 hours
-            saveUninitialized: false
-          }))
-          
-        // Setup requests format parsing (Only JSON requests will be valid)
-        this.app.use(bodyParser.urlencoded({ extended: true }))
-        this.app.use(bodyParser.json())
-
-        // Middleware to protect against HTTP Parameter Pollution attacks
-        this.app.use(hpp())
-    }
-
-    private initializeRouting(): void {
-
-        this.app.use('/', new AuthRoute().router)
-        this.app.use('/', new CartRoute().router)
-        this.app.use('/', new CartSetRoute().router)
-    }
-
-    public listen() {
-        
-        // Start listening on the specified Port 
-        this.app.listen(process.env.PORT, () => {
-            console.log(`Server is listening PORT 3000`)
-        })
-    }
-}
-
-const app: Server = new Server
 app.listen()
